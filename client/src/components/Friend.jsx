b49de014203ab083e97ea64e497bec0698652435
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-07-09 18:46:56 +0530

    bug fixed

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -64,16 +64,21 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
           </Typography>
         </Box>
       </FlexBetween>
-      <IconButton
-        onClick={() => patchFriend()}
-        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
-      >
-        {isFriend ? (
-          <PersonRemoveOutlined sx={{ color: primaryDark }} />
-        ) : (
-          <PersonAddOutlined sx={{ color: primaryDark }} />
-        )}
-      </IconButton>
+      {
+        friendId === _id ?
+          <i></i>
+          :
+          <IconButton
+            onClick={() => patchFriend()}
+            sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+          >
+            {isFriend ? (
+              <PersonRemoveOutlined sx={{ color: primaryDark }} />
+            ) : (
+              <PersonAddOutlined sx={{ color: primaryDark }} />
+            )}
+          </IconButton>
+      }
     </FlexBetween>
   );
 };
