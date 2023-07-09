36cebe18d15b56b976eaa57fc43f0ccda799bc6d
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-07-09 18:20:07 +0530

    state in app

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,51 +1,47 @@
-import { Palette } from "@mui/icons-material";
-import {createSlice} from "@reduxjs/toolkit";
-import { act } from "react-dom/test-utils";
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState={
-    mode:"light",
-    user: null,
-    token:null,
-    posr:[],
-}
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
 
-export const authSlice= createSlice({
-    name:"auth",
-    initialState,
-    reducers:{
-         setMode:(state)=>{
-            state.mode=state.mode==="light" ? "dark" : "ligth";
-         },
-         setLogin:(state,action)=>{
-            state.user=action.payload.user;
-            state.token=action.payload.token;
-         },
-         setLogout:(state)=>{
-            state.user=null;
-            state.token=null;
-         }, 
-         setFriends:(state,action)=>{
-            if(state.user){
-                state.user.friends=action.payload.friends;
-            }
-            else{
-                console.error("user friends non-existent :(")
-            }
-         },
-         setPosts:(state,action)=>{
-            state.posts=action.payload.pots;
-         },
-         setPost:(state,action)=>{
-            const updatePosts=state.post.map((post)=>{
-                if(post._id===action.payload.post._id)return action.payload.post;
-                return post;
-            });
-            state.post=updatePosts;
-         }
-    }
-})
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("user friends non-existent :(");
+      }
+    },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
+  },
+});
 
-
-export const{setMode,setLogin,setLogout,setFriends,setPost,setPosts}=authSlice.actions;
-
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
 export default authSlice.reducer;
