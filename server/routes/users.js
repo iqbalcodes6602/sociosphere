9d9c4f425c9362538f0cb9077e5daaa1edbc6339
iqbal <111554475+iqbalcodes6602@users.noreply.github.com>
2023-07-30 23:39:03 +0530

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,11 +8,11 @@ import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-/* READ */
+/* READ USER*/
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
 
-/* UPDATE */
+/* UPDATE USER*/
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
